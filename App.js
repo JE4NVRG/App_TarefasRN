9d4af7237dd329509545f380639c8ab45379c633
Jean Carlos Vargas <106420077+JE4NVRG@users.noreply.github.com>
2022-06-18 01:25:01 -0300

    iniciando o app

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , ImageBackground} from 'react-native';
 
 export default function App() {
+
+  const image = require('./resources/bg.jpg');
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex:1}}>
+      
+        <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+        <View style={styles.coverView}><Text style={styles.textHeader}>Lista de Tarefas - JE4N CARLOS</Text></View>
+          
+        </ImageBackground>
     </View>
+  
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  image: {
+    width: '100%',
+    height: 150,
+    resizeMode: 'cover',
+    
   },
+  coverView: {
+    width: '100%',
+    height: 150,
+    backgroundColor: 'rgba(0,0,0,0.5)'
+  },
+  textHeader: {
+    textAlign: 'center',
+    color: '#fff',
+    fontSize: 20,
+    marginTop: 20,
+    paddingTop: 50,
+  }
+ 
 });
